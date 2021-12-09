094724878a24a60f6dc894a2b7fcc6d45ad002a4
jordi <jordigraurios@gmail.com>
2021-12-09 20:01:48 +0100

    start myTeam

--- src/js/functions/playerFunctions.js
+++ src/js/functions/playerFunctions.js
@@ -40,3 +40,26 @@ export async function getPlayerResponse(playerId) {
         return { status: false, msg: e };
     }
 }
+
+export function servePlayers(players, ul) {
+    ul.innerHTML = '';
+    const playerList = `
+    <li name="$$ITEM_ID$$" class='list-group-item player_item'>
+        <div>
+            <img src="$$ITEM_PHOTO$$">
+            <a>$$ITEM_NAME$$</a>
+        </div>
+    </li>
+    `;
+    for (const player of players) {
+        // const playerObj = {
+        //     id: players[i].player.id,
+        //     name: players[i].player.name,
+        //     photo: players[i].player.photo,
+        // };
+        // console.log(JSON.stringify(playerObj));
+        // const player = new Player(playerObj);
+        const replacedItemHTML = playerList.replace('$$ITEM_ID$$', player.id).replace('$$ITEM_PHOTO$$', player.photo).replace('$$ITEM_NAME$$', player.name);
+        ul.insertAdjacentHTML('beforeend', replacedItemHTML);
+    }
+}
